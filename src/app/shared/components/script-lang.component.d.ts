ff228aa422963f1abbc5c12b928dac6d073eeac0
Igor Kulikov <ikulikov@thingsboard.io>
2022-10-28 18:07:13 +0300

    Update types

---
+++ src/app/shared/components/script-lang.component.d.ts
@@ -0,0 +1,24 @@
+import { OnInit } from '@angular/core';
+import { ControlValueAccessor, FormBuilder, FormGroup } from '@angular/forms';
+import { PageComponent } from '@shared/components/page.component';
+import { Store } from '@ngrx/store';
+import { AppState } from '@core/core.state';
+import { ScriptLanguage } from '@shared/models/rule-node.models';
+import * as i0 from "@angular/core";
+export declare class TbScriptLangComponent extends PageComponent implements ControlValueAccessor, OnInit {
+    protected store: Store<AppState>;
+    private fb;
+    scriptLangFormGroup: FormGroup;
+    scriptLanguage: typeof ScriptLanguage;
+    disabled: boolean;
+    private propagateChange;
+    constructor(store: Store<AppState>, fb: FormBuilder);
+    ngOnInit(): void;
+    registerOnChange(fn: any): void;
+    registerOnTouched(fn: any): void;
+    setDisabledState(isDisabled: boolean): void;
+    writeValue(scriptLang: ScriptLanguage): void;
+    updateView(scriptLang: ScriptLanguage): void;
+    static ɵfac: i0.ɵɵFactoryDeclaration<TbScriptLangComponent, never>;
+    static ɵcmp: i0.ɵɵComponentDeclaration<TbScriptLangComponent, "tb-script-lang", never, { "disabled": "disabled"; }, {}, never, never>;
+}
